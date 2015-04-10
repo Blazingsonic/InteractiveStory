2eb07de7bc09e7850dca54a2498d83aef64498ea
Sebastian Saier <sebastian.saier@web.de>
2015-04-10 21:04:00 +0200

    new files

---
+++ app/src/main/java/com/saier/sebastian/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.saier.sebastian.interactivestory.model;
+
+/**
+ * Created by Sebastian on 20.03.2015.
+ */
+public class Choice {
+
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
